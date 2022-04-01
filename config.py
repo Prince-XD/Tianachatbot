697e4237866ed25de6454ae450e77307a9091bc6
PRAJAPATI PRINCE <79126372+prince301102@users.noreply.github.com>
2022-04-01 13:06:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 ARQ_API_KEY = "CIIUHX-TKZMEQ-LPZFEQ-ZUUFJO-ARQ" 
 LANGUAGE = "en"
-ARQ_API_BASE_URL = "https://thearq.tech"
+ARQ_API_BASE_URL = "https://arq.hamker.in"
 
 bot = Client(":memory:",
              api_id=API_ID,
